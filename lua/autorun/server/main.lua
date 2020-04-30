169c998c02fd4516856481aee9adcf363c95dce8
Lambert Teisserenc <lambert.teisserenc@braineet.com>
2020-05-01 00:20:27 +0200

    first commit

---
+++ lua/autorun/server/main.lua
@@ -0,0 +1,19 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lambert
+-- Date: 5/1/20
+-- Time: 12:09 AM
+--
+
+print("refresh")
+
+local function playDeathSound(location)
+    sound.Play( "sound/aaaahh.wav", location)
+end
+
+hook.Add("PlayerDeath", "MyFirstScript", function(victim, inflictor, attacker)
+    print("test")
+    print(victim, inflictor, attacker)
+    playDeathSound(victim:GetEntityInUse():GetPos())
+end)
+
